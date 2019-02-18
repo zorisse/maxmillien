1b6895babf7ed70eeec49acc3fe87f847ca91bcb
Nouri T <zorisse@gmail.com>
2019-02-18 22:15:26 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-//Import un component 
-import Person from './Person/Person'
+import React, { Component } from "react";
+import "./App.css";
+//Import un component
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "lapinouState ", age: 34 },
+      { name: "MarleneState ", age: 14 },
+      { name: "Kevin ", age: 3 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>SALUT </h1>
-        <Person name='Nouri' age='28' />
-        <Person name='Lara' age='25' > Props children  : </Person>
-        <Person name='Delphine' age='29' />
+        <Person name="Nouri" age="28" />
+        <Person name="Lara" age="25">
+          {" "}
+          Props children :{" "}
+        </Person>
+        <Person name="Delphine" age="29" />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
       </div>
     );
   }
