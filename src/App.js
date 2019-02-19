fd392cc538a600c61dafb648dc15c3c710d651ee
Nouri TALEB <ntaleb@biocome.local>
2019-02-19 13:51:11 +0100

    setSate()

--- src/App.js
+++ src/App.js
@@ -4,31 +4,78 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  // State 
   state = {
     persons: [
       { name: "lapinouState ", age: 34 },
       { name: "MarleneState ", age: 14 },
       { name: "Kevin ", age: 3 }
-    ]
+    ],
+    toggler: false
   };
+
+// Méthode 
+
+nameSwitchHandler = (event) => {
+  // this.setSate() est une méthode.
+
+  console.log("debut" + this.state.toggler);
+
+  if (this.state.toggler) {
+
+    this.setState({
+      persons: [
+        { name: "autre Sate ", age: 34 },
+      { name: "autre state ", age: 14 },
+      { name: "AutreState ", age: 3 }
+
+      ],
+      toggler: false
+    });
+
+
+  } else {
+    this.setState({
+      persons: [
+        { name: "lapinouState ", age: 34 },
+      { name: "MarleneState ", age: 14 },
+      { name: "Kevin ", age: 3 }
+
+      ],
+      toggler: true
+    });
+
+
+  }
+  console.log("Fin " + this.state.toggler);
+}
+
+
+
+
   render() {
     return (
       <div className="App">
-        <h1>SALUT </h1>
-        <Person name="Nouri" age="28" />
-        <Person name="Lara" age="25">
-          {" "}
-          Props children :{" "}
-        </Person>
-        <Person name="Delphine" age="29" />
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
+        <div id='L1'>
+          <h1>SALUT </h1>
+          <Person name="Nouri" age="28" />
+          <Person name="Lara" age="25">
+            {" "}
+            Props children :{" "}
+          </Person>
+          <Person name="Delphine" age="29" />
+        </div>
+        <div id=''>
+        <button onClick= {this.nameSwitchHandler}> Click Moi </button>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+          />
+        </div>
       </div>
     );
   }
